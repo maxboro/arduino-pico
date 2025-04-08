4c5f82a7458fd3b8d4e107d1db3ebad2c35eec64
Max B. <77401015+maxboro@users.noreply.github.com>
2025-04-08 20:50:03 +0300

    Add LED (#2)

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Description
-Basic Arduino project for Raspberry Pi Pico
+Basic Arduino project for Raspberry Pi Pico.
+Turns ON LED on random value higher than threshold.
 
 # Credits
 I used [Wokwi](https://wokwi.com/) to simulate the hardware.
+
+# Example
+![screenshot](./docs/screenshot.png)
--- sketch.ino
+++ sketch.ino
@@ -1,9 +1,27 @@
+#define INDICATOR_PIN 4
+#define MIN_VAL 0
+#define MAX_VAL 100
+#define INDICATOR_BOUND 50
+
 void setup() {
   Serial1.begin(115200);
-  Serial1.println("Hello, Raspberry Pi Pico!");
+  pinMode(INDICATOR_PIN, OUTPUT);
+
+  Serial1.println("Raspberry Pi Pico is set!");
+}
+
+// Switch on led on value higher than threshold 
+void update_led(int value) {
+  if (value > INDICATOR_BOUND){
+    digitalWrite(INDICATOR_PIN, HIGH);
+  } else {
+    digitalWrite(INDICATOR_PIN, LOW);
+  }
 }
 
 void loop() {
-  Serial1.println("I am running in the loop");
-  delay(1000);
+  int rand_val = random(MIN_VAL, MAX_VAL);
+  Serial1.println("Random value is: " + String(rand_val));
+  update_led(rand_val);
+  delay(7000);
 }
